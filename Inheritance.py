1b9ccf3bde51864899f1b2b6c7d5beef44970583
krishanavata <44390451+krishanavata@users.noreply.github.com>
2018-10-25 12:48:57 +0530

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,29 @@
+
+class Bird:
+    def __init__(self,type=None,color=None):
+        pass
+    def canFly(self):
+        pass
+
+class Parrot(Bird):
+    def __init__(self):
+        pass
+    def canFly(self):
+        print("yes can fly")
+
+
+class Pangvinue(Bird):
+    def __init__(self):
+        pass
+    def canFly(self):
+        print("yes can't fly")
+
+
+def isFly(bird):
+    bird.canFly()
+
+p=Pangvinue()
+p1=Parrot()
+
+isFly(p)
+isFly(p1)
